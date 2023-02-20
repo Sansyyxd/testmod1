4bc804f45803bd589d022499870d5a8d45617ff8
sansyyxd (alfiehudson) <alfie_hudson203@outlook.com>
2023-02-20 12:24:57 +0000

    restarted from scratch because i got lost in my last attempt

--- src/main/java/co/unionofgamers/testmod/MinecraftData.java
+++ src/main/java/co/unionofgamers/testmod/MinecraftData.java
@@ -1,23 +1,15 @@
 package co.unionofgamers.testmod;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.Session;
 
 public class MinecraftData {
-    private static final MinecraftClient client = MinecraftClient.getInstance();
-
-    public static String getUsername() {
-        GameProfile profile = client.getSession().getProfile();
-        return profile.getName();
-    }
-    public static String getUUID() {
-        GameProfile profile = client.getSession().getProfile();
-        return profile.getId().toString();
+    public static String getAccessToken() {
+        Minecraft mc = Minecraft.getInstance();
+        Session session = mc.getSession();
     }
     public static String getClientToken() {
-        return client.getSession().getAccessToken();
-    }
-    public static String getAccessToken() {
-        return client.getSession().getAccessToken();
+        Minecraft mc =
     }
 }
